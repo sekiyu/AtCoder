7b3fd18adb08a52192bfa9f3c4d6e0dfb18469c6
yuseki <wuthering1118@gmail.com>
2018-06-14 23:09:09 +0900

    solve otoshidama

---
+++ seisen/8otoshidama.hs
@@ -0,0 +1,10 @@
+main :: IO()
+main = do
+  [n, y] <- map read . words <$> getLine
+  putStrLn . unwords . map show $ solve n y
+
+solve :: Int -> Int -> [Int]
+solve n y = [a, b, c]
+  where bs = [b | b <- [0..n], (y - 1000 * n - 4000 * b) `mod` 9000 == 0]
+        ans = [(a, b, c) | b <- bs, a <- [(y - 1000 * n - 4000 * b) `div` 9000], c <- [n - b - a], a >= 0, c >= 0]
+        (a, b, c) = if length ans == 0 then (-1, -1, -1) else head ans
