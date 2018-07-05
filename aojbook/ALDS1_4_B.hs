73b2dd8a91be56033bbc49c9ec7b310472db3d9b
utopian <wuthering1118@gmail.com>
2018-07-05 23:55:37 +0900

    TLE: even built-in function elem doesn't pass ALDS1_4_B
    The cause maybe the IO

--- aojbook/ALDS1_4_B.hs
+++ aojbook/ALDS1_4_B.hs
@@ -1,18 +1,23 @@
+import Data.List
+
 main :: IO()
 main = do
   _ <- fmap read getLine :: IO Int
   ss <- fmap (map read . words) getLine :: IO [Int]
   _ <- fmap read getLine :: IO Int
   ts <- fmap (map read . words) getLine :: IO [Int]
-  print $ solve ss ts
+  print $ solve ss ts 0
 
-solve :: [Int] -> [Int] -> Int
-solve ss = foldl (\acc t -> acc + binarySearch t ss 0 (length ss - 1)) 0
+solve :: [Int] -> [Int] -> Int -> Int
+--solve ss = foldl' (\acc t -> acc + binarySearch t ss 0 (length ss - 1)) 0
+--solve ss ts = length [1 | t <- ts, binarySearch t ss 0 (length ss - 1)]
+solve _ [] n = n
+solve ss (t:ts) n = solve ss ts $ n + if elem t ss then 1 else 0
 
-binarySearch :: Int -> [Int] -> Int -> Int -> Int
+binarySearch :: Int -> [Int] -> Int -> Int -> Bool
 binarySearch t ss b e
-  | t == mid = 1
-  | b == e || b > e  = 0
+  | t == mid = True
+  | b == e || b > e  = False
   | t < mid = binarySearch t ss b (i - 1)
   | t > mid = binarySearch t ss (i + 1) e
   where
