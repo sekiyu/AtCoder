cdd27c499c5b3bc6ff1bc04b65fc139cbe2d9621
utopian <wuthering1118@gmail.com>
2018-07-26 23:37:15 +0900

    refactor: improve ALDS1_11_D but still TLE

--- aojbook/ALDS1_11_D.hs
+++ aojbook/ALDS1_11_D.hs
@@ -1,18 +1,23 @@
 import Control.Monad
 import qualified Data.Map.Strict as Map
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main = do
   (n:m:_) <-  fmap (map read . words) getLine :: IO [Int]
   gs <- replicateM m $ fmap (map read . words) getLine :: IO [[Int]]
-  q <- readLn
+  q <- readLn :: IO Int
   qs <- replicateM q $ fmap (map read . words) getLine :: IO [[Int]]
+
   let al = toAdjList gs
   putStr . unlines . map
-    (\(a:b:_) -> if reachable [] a b al then "yes" else "no") $ qs
+    (\(a:b:_) -> if reachable' [] a [b] al then "yes" else "no") $ qs
+    --(\(a:b:_) -> if reachable [] a b al then "yes" else "no") $ qs
 
 type AdjList = Map.Map Int [Int]
 
+-- first submission TLE@13
 reachable :: [Int] -> Int -> Int -> AdjList -> Bool
 reachable vs a b al
   | friendsOfB == [] = False
@@ -22,6 +27,15 @@ reachable vs a b al
 
 --多分anyの中で重複した探索をしてしまっている。
 --訪問済みリストの他に、訪問予定リストも持つようにする
+
+-- second submission TLE@18
+reachable' :: [Int] -> Int -> [Int] -> AdjList -> Bool
+reachable' _ _ [] _ = False
+reachable' vs a (b:bs) al
+  | elem a friendsOfB = True
+  | otherwise = reachable' (b:vs) a (friendsOfB ++ bs) al
+    where
+      friendsOfB = filter (`notElem` vs) $ getAdjs b al
 
 toAdjList :: [[Int]] -> AdjList
 toAdjList qs = insertAdj qs Map.empty
