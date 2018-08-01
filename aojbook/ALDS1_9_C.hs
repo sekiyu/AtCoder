e879f806d032115c242259a767af4d2ef452bb09
utopian <wuthering1118@gmail.com>
2018-08-02 00:05:11 +0900

    perf: use bang pattern to priority queue, but not improve performance

--- aojbook/ALDS1_9_C.hs
+++ aojbook/ALDS1_9_C.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE BangPatterns #-}
 import Control.Monad
 import Data.Functor
 
 main :: IO ()
-main = solve Empty
+-- main = solve Empty
+main = do
+   ans <- solve' Empty []
+   putStr . unlines . map show . reverse $ ans
 
 solve :: MaxHeap Int -> IO ()
-solve heap = do
+solve !heap = do
   c <- words <$> getLine :: IO [String]
   case (head c) of
     "end" -> return ()
@@ -14,11 +18,20 @@ solve heap = do
       print v
       solve t
 
+solve' :: MaxHeap Int -> [Int] -> IO [Int]
+solve' !heap !as = do
+  c <- words <$> getLine :: IO [String]
+  case (head c) of
+    "end" -> return as
+    "insert" -> solve' (insert (read . last $ c) heap) as
+    "extract" -> let (v, t) = extract heap in solve' t (v:as)
+
+
 data MaxHeap a = Empty | Tree a (MaxHeap a) (MaxHeap a) deriving Show
 
 insert :: (Ord a) => a -> MaxHeap a -> MaxHeap a
 insert a Empty = Tree a Empty Empty
-insert a t = merge (Tree a Empty Empty) t
+insert a !t = merge (Tree a Empty Empty) t
 
 extract :: (Ord a) => MaxHeap a -> (a, MaxHeap a)
 extract (Tree x left right) = (x, merge left right)
@@ -26,6 +39,6 @@ extract (Tree x left right) = (x, merge left right)
 merge :: (Ord a) => MaxHeap a -> MaxHeap a -> MaxHeap a
 merge t Empty = t
 merge Empty t = t
-merge a@(Tree x xl xr) b@(Tree y yl yr)
+merge a@(Tree !x !xl !xr) b@(Tree !y !yl !yr)
   | x < y     = Tree y yr (merge yl a)
   | otherwise = Tree x xr (merge xl b)
