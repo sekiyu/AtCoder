71bf88ca74e322263e15da3d6e5faab625358683
utopian <wuthering1118@gmail.com>
2018-08-16 22:21:16 +0900

    ABC105: solve c

--- ABC105/c.hs
+++ ABC105/c.hs
@@ -3,10 +3,13 @@ import Control.Monad
 main :: IO ()
 main = do
   n <- readLn :: IO Int
-  putStrLn . showbm2 . solve $ n
-  -- print . limn $ n
+  -- putStrLn . showbm2 . solve $ n
+  putStrLn . showbm2 . reverse . solve' $ n
 
+-- brute force search
+-- examine all the candidates
 solve :: Int -> [Int]
+solve 0 = [0]
 solve n = head . join $ [ [ cand | cand <- greedCandidates i, bm2 cand == n]| i <- [0..]]
 
 greedCandidates n = replicateM n [0, 1]
@@ -14,8 +17,12 @@ greedCandidates n = replicateM n [0, 1]
 bm2 as = sum $ zipWith (\a i -> a * (-2) ^ i) (reverse as) [0..]
 
 showbm2 :: [Int] -> String
-showbm2 as = concat $ map show as 
+showbm2 as = concat $ map show as
 
-
-
-limn n = head [i | i <- [0..], 2 ^ i > abs n]
+-- solve according to the commentary
+solve' :: Int -> [Int]
+solve' 0 = [0]
+solve' 1 = [1]
+solve' n = a : solve' ((n - a) `div` (-2))
+  where
+    a = n `mod` 2
