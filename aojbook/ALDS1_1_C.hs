d1b3f28ece5474dce058d96b9cc0d0ff5b6c5f14
utopian <wuthering1118@gmail.com>
2018-08-21 23:27:59 +0900

    ALDS1_1_C: solve prime number

---
+++ aojbook/ALDS1_1_C.hs
@@ -0,0 +1,14 @@
+import Control.Monad
+import Data.Functor
+
+main :: IO ()
+main = do
+  n <- readLn
+  as <- join <$> (replicateM n $ map read . words <$> getLine) :: IO [Int]
+  print . length . filter isPrime $ as
+
+
+isPrime :: Int -> Bool
+isPrime 1 = False
+isPrime 2 = True
+isPrime n = all (\i -> not $ n `mod` i == 0) [2..(1 + (truncate . sqrt $ fromIntegral n))]
