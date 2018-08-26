be3f8610e86842d75520f365841186faa12a47fd
utopian <wuthering1118@gmail.com>
2018-08-26 23:53:00 +0900

    ABC003 D: partial clear, div 10^9 + 7

---
+++ AtCoderProblemsD/ABC003.hs
@@ -0,0 +1,33 @@
+import Control.Monad
+
+main :: IO ()
+main = do
+  (r:c:_) <- fmap (map read . words) getLine :: IO [Integer]
+  (x:y:_) <- fmap (map read . words) getLine :: IO [Integer]
+  (d:l:_) <- fmap (map read . words) getLine :: IO [Integer]
+  let movingPattern = (r - x + 1) `modmul` (c - y + 1)
+  print $  movingPattern `modmul` (count x y d l)
+
+divConst = 10^9 + 7
+
+factorial n
+  | n <= 1 = 1
+  | otherwise = n `modmul` factorial (n - 1)
+invFactorial n
+  | n <= 1 = 1
+  | otherwise = (invFactorial (n - 1)) `moddiv` n
+
+count x y d l = ac `modmul` dc `modmul` lc `modmul` nc
+  where
+    ac = factorial (x `modmul` y)
+    dc = invFactorial d
+    lc = invFactorial l
+    nc = invFactorial $ (x * y) - d - l
+
+modmul x y = ((x `mod` divConst) * (y `mod` divConst)) `mod` divConst
+moddiv x y = modmul x (power y (divConst - 2))
+power x y
+  | y == 0 = 1
+  | y == 1 = x `mod` divConst
+  | y `mod` 2 == 0 = (power x (y `div` 2))^2 `mod` divConst
+  | otherwise = (power x (y `div` 2))^2 * x `mod` divConst
