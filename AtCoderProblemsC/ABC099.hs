3cdd7f3116dc187d699935eaa3bd3e22861c9c9f
utopian <wuthering1118@gmail.com>
2018-09-04 23:15:43 +0900

    ABC099 C: another solution with division of n and n digits

--- AtCoderProblemsC/ABC099.hs
+++ AtCoderProblemsC/ABC099.hs
@@ -4,32 +4,12 @@ import           Data.Maybe  (fromJust)
 main :: IO ()
 main = do
   n <- readLn
---  print $ solve n 0
-  print $ solve'' n
+  print $ solve' n
 
-solve :: Int -> Int -> Int
-solve 0 i = i
-solve n i = solve (n - k) (i + 1)
-  where
-   k = maximum . filter (n >= ) $ candidates
-
-candidates = a ++ b ++ c
-a = [1]
-b = filter (<= 10^5) (map (6^) [1..6])
-c = filter (<= 10^5) (map (9^) [1..9])
-
-solve' :: Int -> Int
-solve' 0 = 0
-solve' n
-  | elem n candidates = 1
-  | otherwise = 1 + (solve' (last next))
-  -- | otherwise = 1 + (minimum $ map solve' next)
-    where
-      next = filter (0 <) . map (n - ) $ candidates
--- maximum . map (\m -> 1 + solve' m) . filter (0 <) . map (n - ) $ candidates
-
-solve'' :: Int -> Int
-solve'' n = bfs 0 $ IntSet.singleton n
+-- 幅優先探索
+-- 探索の深さだけ覚えていれば良いのでIntSetを使った
+solve :: Int -> Int
+solve n = bfs 0 $ IntSet.singleton n
   where
     bfs i visited
       | IntSet.member 0 visited = i
@@ -37,3 +17,18 @@ solve'' n = bfs 0 $ IntSet.singleton n
 
     visit :: [Int] -> IntSet.IntSet -> IntSet.IntSet
     visit as vs = IntSet.fromList $ [v - a | a <- as, v <- (IntSet.elems vs), v - a >= 0]
+
+    candidates = a ++ b ++ c
+    a = [1]
+    b = filter (<= 10^5) (map (6^) [1..6])
+    c = filter (<= 10^5) (map (9^) [1..9])
+
+-- 別解　Nを6進数と9進数の和で表現するパターンを全探索
+solve' :: Int -> Int
+solve' n = minimum $ map sum $ [(ndigits 6 m) ++ (ndigits 9 (n - m)) | m <- [0..n]]
+
+ndigits :: Int -> Int -> [Int]
+ndigits k n = reverse $ inner n k
+  where
+    inner 0 _ = []
+    inner n k = (n `mod` k):inner (n `div` k) k
