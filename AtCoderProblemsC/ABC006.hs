8eea0034f7d811609751eb9cc7179752fa7d7eb6
utopian <wuthering1118@gmail.com>
2018-09-08 00:06:33 +0900

    ABC006 C: AC

--- AtCoderProblemsC/ABC006.hs
+++ AtCoderProblemsC/ABC006.hs
@@ -1,7 +1,7 @@
 main :: IO ()
 main = do
   (n:m:_) <- fmap (map read . words) getLine :: IO [Int]
-  putStrLn . unwords . map show $ solve n m
+  putStrLn . unwords . map show $ solve' n m
 
 
 -- naive solution
@@ -9,3 +9,12 @@ solve :: Int -> Int -> [Int]
 solve n m
   | n * 2 > m || n * 4 < m = [-1, -1, -1]
   | otherwise = head [[x, y, z] | x <- [0..n], y <- [0..(n - x)], z <- [0..(n - x - y)], 2 * x + 3 * y + 4 * z == m, x + y + z == n]
+
+solve' :: Int -> Int -> [Int]
+solve' n m
+  | n * 2 > m || n * 4 < m = [-1, -1, -1]
+  | otherwise = if rest < 0
+                then [abs rest, n + rest, 0]
+                else [0, n - rest, rest]
+    where
+      rest = m - n * 3
