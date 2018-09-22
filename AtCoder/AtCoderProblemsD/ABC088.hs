e439e7a2cb6125ddd529711c765de9ad093f1e7f
utopian <wuthering1118@gmail.com>
2018-09-23 03:49:37 +0900

    ABC088 D: TLE

--- AtCoder/AtCoderProblemsD/ABC088.hs
+++ AtCoder/AtCoderProblemsD/ABC088.hs
@@ -1,31 +1,32 @@
--- ABC007 C Breadth First Search
+-- ABC088 D Grid Repainting
 import Control.Monad
 import qualified Data.Set as Set
+import Data.Maybe
 import Data.Array
-import qualified Data.Map.Strict as Map
+import Data.List
+import Debug.Trace
 
 main :: IO ()
 main = do
-  (r:c:_) <- fmap (map read . words) getLine :: IO [Int]
-  (sx:sy:_) <- fmap (map read . words) getLine :: IO [Int]
-  (gx:gy:_) <- fmap (map read . words) getLine :: IO [Int]
-  ms <- replicateM r $ getLine :: IO [[Char]]
-  print $ solve (sx,sy) (gx,gy) $ toMaze (r, c) ms
+  (h:w:_) <- fmap (map read . words) getLine :: IO [Int]
+  ms <- replicateM h $ getLine :: IO [[Char]]
+  -- print $ toMaze (h, w) ms
+  print . solve (1, 1) (h, w) . toMaze (h, w) $ ms
 
 type Position = (Int, Int)
 type Maze = Array Position Bool
 toMaze :: (Int, Int) -> [[Char]] -> Maze
 toMaze (n, m) = listArray ((1,1), (n,m)) . join . map (map (=='.'))
 
-
 solve :: Position -> Position -> Maze -> Int
-solve start goal maze = bfs (Set.singleton start) 0
+solve start goal maze = n - 1 - (bfs (Set.singleton start) 0)
   where
-    -- initial = listArray (bounds maze) (repeat maxBound)
+    n = length . filter id $ elems maze
     bfs :: Set.Set Position -> Int -> Int
     bfs visits n
       | goal `Set.member` visits = n
       | otherwise = bfs (Set.fromList . join . map step $ Set.toList visits) (n + 1)
+      -- | otherwise = traceShow visits $ bfs (Set.fromList . join . map step $ Set.toList visits) (n + 1)
 
     step :: Position -> [Position]
     step (x,y) = validate [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
