598db313bbb65ef8e5e90e0502f640f3b39aae79
utopian <wuthering1118@gmail.com>
2018-10-06 15:35:03 +0900

    ABC009 C:AC

--- AtCoder/AtCoderProblemsC/ABC009.hs
+++ AtCoder/AtCoderProblemsC/ABC009.hs
@@ -1,10 +1,5 @@
-import Control.Monad
-import Data.Maybe
-import Debug.Trace
+-- ABC009 C - 辞書式順序ふたたび
 import Data.List
-import qualified Data.Map.Strict as Map
-import qualified Data.IntMap.Strict as IntMap
-import qualified Data.Set as Set
 
 main :: IO ()
 main = do
@@ -13,7 +8,35 @@ main = do
   putStrLn $ solve k s
 
 solve :: Int -> String -> String
-solve k s = head . sort . filter validate $ permutations s
+solve k s = go k s sorted
+  where
+    sorted = sort s
+    go :: Int -> String -> String -> String
+    go _ [] _ = []
+    go _ _ [] = []
+    go k (s:ss) (h:hh)
+      | k > n = h:hh -- kの方が大きければsortしたものを返す
+      | s == h = s:go k ss (sort hh)
+      | k + count >= n = h:go (k - 1) ss (sort hh)
+      | otherwise = go k (s:ss) (hh ++ [h])
+      where
+        n = 1 + length ss
+        count = numCommonElements ss hh
+
+numCommonElements :: (Ord a) => [a] -> [a] -> Int
+numCommonElements as bs = inner (sort as) (sort bs)
+  where
+    inner :: (Ord a) => [a] -> [a] -> Int
+    inner [] _ = 0
+    inner _ [] = 0
+    inner (a:as) (b:bs)
+      | a == b = 1 + inner as bs
+      | a > b = inner (a:as) bs
+      | a < b = inner as (b:bs)
+
+
+naive :: Int -> String -> String
+naive k s = head . filter validate . sort . permutations $ s
   where
     validate :: String -> Bool
     validate ss = (length . filter not $ zipWith (==) ss s) <= k
