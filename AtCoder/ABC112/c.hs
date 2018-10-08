c6cc03a8f0bf0029da138cd12fb1cf8ace791b33
utopian <wuthering1118@gmail.com>
2018-10-08 18:54:56 +0900

    ABC112 C:refactor to calculate efficiently

--- AtCoder/ABC112/c.hs
+++ AtCoder/ABC112/c.hs
@@ -1,5 +1,6 @@
 -- ABC112 C - Pyramid
 import Control.Monad
+import Data.List
 
 main :: IO ()
 main = do
@@ -8,14 +9,18 @@ main = do
   putStrLn . unwords . map show $ solve am
 
 solve :: [[Int]] -> [Int]
-solve am = head . filter isOk $ [[x,y,h] | (x,y) <- candidates, h <- heightCandidates]
+solve am = head . filter isOk $ [[x,y,centerH (x,y)] | x <- [0..100], y <- [0..100]]
   where
-    maxHeight = maximum . map (!!2) $ am
-    heightCandidates = [(maxHeight - 200)..(maxHeight + 200)]
     isOk (cx:cy:ch:_) = all (\(x:y:h:_) -> height ch (x,y) (cx,cy) == h) am
+    highest = last $ sortBy (\a b -> compare (a!!2) (b!!2)) am
+    centerH = calcBackHeight highest
 
 type Position = (Int, Int)
 height :: Int -> Position -> Position -> Int
 height h (x, y) (cx, cy) = max (h - (abs (x - cx)) - (abs (y - cy))) 0
-
-candidates = [ (x,y)| x <- [0..100], y <- [0..100]]
+calcBackHeight :: [Int] -> Position -> Int
+calcBackHeight a (cx, cy) = h + (abs (x - cx)) + (abs (y - cy))
+  where
+    x = a !! 0
+    y = a !! 1
+    h = a !! 2
