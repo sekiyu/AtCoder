a6df7376a7d31bb4a9f24ba12917148d23023025
utopian <wuthering1118@gmail.com>
2018-10-28 15:12:45 +0900

    ABC032 D: add DP solution but TLE

--- AtCoder/AtCoderProblemsD/ABC032.hs
+++ AtCoder/AtCoderProblemsD/ABC032.hs
@@ -1,19 +1,32 @@
 -- ABC032 D - ナップサック問題
 import Control.Monad
-import Data.Maybe
-import Debug.Trace
 import Data.List
-import qualified Data.Map.Strict as Map
-import qualified Data.IntMap.Strict as IntMap
-import qualified Data.Set as Set
-import qualified Data.IntSet as IntSet
+import qualified Data.IntMap.Strict as M
+-- import qualified Data.Map.Strict as M
 
 main :: IO ()
 main = do
   (n:w:_) <- fmap (map read . words) getLine :: IO [Int]
+  -- vws <- replicateM (fromIntegral n) $ (\(v:w:_) -> (v, w)) . map (toInteger . read) . words <$> getLine :: IO [(Integer, Integer)]
   vws <- replicateM n $ (\(v:w:_) -> (v, w)) . map read . words <$> getLine :: IO [(Int, Int)]
-  print $ solve w vws
+  -- print $ solve w vws
+  print $ solveMapDp w vws
 
+
+-- DP solution using IntMap
+solveMapDp :: Int -> [(Int, Int)] -> Int
+solveMapDp maxw vws = maximum . M.elems $ dp
+  where
+    initial = M.singleton 0 0
+    dp = foldr f initial vws
+    -- dp = foldl' (flip f) initial vws
+    f :: (Int, Int) -> M.IntMap Int -> M.IntMap Int
+    f (v, w) table = foldr g table $ M.keys table
+      where
+        g :: Int -> M.IntMap Int -> M.IntMap Int
+        g i table
+          | i + w > maxw = table
+          | otherwise = M.insertWith max (i+w) ((table M.! i) + v) table
 
 -- Definitive solution
 solve :: Int -> [(Int, Int)] -> Int
