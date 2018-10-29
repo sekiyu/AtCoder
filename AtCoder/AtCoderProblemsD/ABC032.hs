9766714d4aec16e900158d8c223a2810814ef7c2
utopian <wuthering1118@gmail.com>
2018-10-30 00:00:00 +0900

    ABC032 D: feat add weight DP

--- AtCoder/AtCoderProblemsD/ABC032.hs
+++ AtCoder/AtCoderProblemsD/ABC032.hs
@@ -3,17 +3,60 @@ import Control.Monad
 import Data.List
 import qualified Data.IntMap.Strict as M
 -- import qualified Data.Map.Strict as M
+-- import Data.Array.Unboxed
+import Data.Array
+import Debug.Trace
 
 main :: IO ()
 main = do
   (n:w:_) <- fmap (map read . words) getLine :: IO [Int]
   -- vws <- replicateM (fromIntegral n) $ (\(v:w:_) -> (v, w)) . map (toInteger . read) . words <$> getLine :: IO [(Integer, Integer)]
   vws <- replicateM n $ (\(v:w:_) -> (v, w)) . map read . words <$> getLine :: IO [(Int, Int)]
-  -- print $ solve w vws
-  print $ solveMapDp w vws
+  let maximumv = maximum . map fst $ vws
+      maximumw = maximum . map snd $ vws
+      f n mv mw
+        | n <= 30 = print $ solveMapDp w vws
+        | mw <= 1000 = print $ solveWeightDp w vws
+        | otherwise = print $ solveValueDp w vws
+  -- f n maximumv maximumw
+  print $ solveValueDp w vws
 
 
--- DP solution using IntMap
+-- solveValueDp :: Int -> [(Int, Int)] -> Int
+solveValueDp maxw vws = snd . fst . last . sortOn snd . filter (\((_, v), w) -> w <= maxw) $ assocs dp
+  where
+    n = length vws
+    maximumv = maximum . map fst $ vws
+    arrvws = listArray (1, n) vws :: Array Int (Int, Int)
+    dp :: Array (Int, Int) Int
+    dp = listArray ((0, 0), (n, n * maximumv)) $ map f [(i, j) | i <- [0..n], j <- [0..(n * maximumv)]]
+    f (0, 0) = 0
+    f (0, _) = maxBound
+    f (i, j)
+      | j < v = dp!(i-1, j)
+      | otherwise = min (dp!(i-1, j)) (dp!(i-1, j - v) + w)
+      where
+        v = fst $ arrvws!i
+        w = snd $ arrvws!i
+
+
+solveWeightDp :: Int -> [(Int, Int)] -> Int
+solveWeightDp maxw vws = dp!(n, maxw)
+  where
+    n = length vws
+    arrvws = listArray (1, n) vws :: Array Int (Int, Int)
+    dp :: Array (Int, Int) Int
+    dp = listArray ((0, 0), (n, maxw)) $ map f [(i, j) | i <- [0..n], j <- [0..maxw]]
+    f (0, _) = 0
+    f (_, 0) = 0
+    f (i, j)
+      | j < w = dp!(i-1, j)
+      | otherwise = max (dp!(i-1, j)) (dp!(i-1, j - w) + v)
+      where
+        v = fst $ arrvws!i
+        w = snd $ arrvws!i
+
+-- Solution using IntMap
 solveMapDp :: Int -> [(Int, Int)] -> Int
 solveMapDp maxw vws = maximum . M.elems $ dp
   where
