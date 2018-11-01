a8d7b91042fd3e5e42d89a62142b1884b0bc778f
utopian <wuthering1118@gmail.com>
2018-11-01 23:42:43 +0900

    MC2018 D: add DP solution but TLE

--- AtCoder/AtCoderProblemsD/MC2018.hs
+++ AtCoder/AtCoderProblemsD/MC2018.hs
@@ -1,4 +1,4 @@
-Maximum cup 2018 D - Many Go Round
+-- Maximum cup 2018 D - Many Go Round
 import Control.Monad
 import Data.Maybe
 import Debug.Trace
@@ -7,21 +7,38 @@ import qualified Data.Map.Strict as Map
 import qualified Data.IntMap.Strict as IntMap
 import qualified Data.Set as Set
 import qualified Data.IntSet as IntSet
-
+import Data.Array
 
 main :: IO ()
 main = do
   (n:m:l:x:_) <- fmap (map read . words) getLine :: IO [Int]
   as <- fmap (map read . words) getLine :: IO [Int]
-  putStrLn $ if solve n m l x as then "Yes" else "No"
+  putStrLn $ if solveDp n m l x as then "Yes" else "No"
 
--- DPでテーブルに何周目かという情報を持たせる予定
 solveDp :: Int -> Int -> Int -> Int -> [Int] -> Bool
-solveDp n m l x as = IntMap.member l dp && dp IntMap.! l <= x
+solveDp n m l x as = traceShow dp $ dp!(n, l) <= x
   where
-    dp = foldr f (IntMap.singleton 0 0) as
-    f :: Int -> IntMap.IntMap -> IntMap.IntMap
-    f _ dp = dp
+    arr = listArray (0, n) (0:as)
+    dp = listArray ((0, 0), (n, m))
+      $ map f [ (i, j)| i <- [0..n], j <- [0..m]] :: Array (Int, Int) Int
+    f :: (Int, Int) -> Int
+    f (_, 0) = 1
+    f (0, _) = maxBound `div` 2
+    f (i, j) = min (dp!(i-1, j))
+      $ dp!(i-1, (j - arr!i) `mod` m) + (j + arr!i) `div` m
+
+
+solveIntMap :: Int -> Int -> Int -> Int -> [Int] -> Bool
+solveIntMap n m l x as = IntMap.member l dp && dp IntMap.! l <= x
+  where
+    dp = foldr f (IntMap.singleton 0 1) as
+    f :: Int -> IntMap.IntMap Int -> IntMap.IntMap Int
+    f a dic = IntMap.foldrWithKey addIfFirst dic dic
+      where
+        addIfFirst k v d = let new = (k + a) `mod` m
+                           in if IntMap.member new d
+                           then IntMap.insertWith min new (v + (k + a) `div` m) d
+                           else IntMap.insert new (v + (k + a) `div` m) d
 
 -- definitive solution
 solve :: Int -> Int -> Int -> Int -> [Int] -> Bool
