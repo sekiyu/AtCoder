1143abe5b8169d61cca02e491ee09f845261e089
utopian <wuthering1118@gmail.com>
2018-12-17 23:12:58 +0900

    AGC029 A : solve

--- AtCoder/AGC029/a.hs
+++ AtCoder/AGC029/a.hs
@@ -3,14 +3,24 @@ main :: IO ()
 main = getLine >>= print . solve . toBools
 
 solve :: [Bool] -> Int
-solve ss
+solve ss = go 0 ss
+  where 
+    go _ [] = 0
+    go i (s:ss)
+      | s == False = go (i+1) ss
+      | otherwise = i + go i ss
+
+toBools [] = []
+toBools (s:ss) = (if s == 'W'
+                 then True
+                 else False):toBools ss
+
+solve' :: [Bool] -> Int
+solve' ss
   | all (not . id) ss = 0
   | otherwise = length a + solve (a ++ b')
     where
       (a, b) = span (not . id) . dropWhile id $ ss
       b' = if null b then [] else (tail b)
 
-toBools [] = []
-toBools (s:ss) = (if s == 'W'
-                 then True
-                 else False):toBools ss
+                 
