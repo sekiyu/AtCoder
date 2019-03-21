78aa2da210d6c5c65ced57841d1b19a5470a725e
utopian <wuthering1118@gmail.com>
2019-03-21 23:04:29 +0900

    ABC118 D

--- AtCoder/ABC118/d.hs
+++ AtCoder/ABC118/d.hs
@@ -13,11 +13,39 @@ main :: IO ()
 main = do
   (n:m:_) <- fmap (map read . words) getLine :: IO [Int]
   as <- fmap (map read . words) getLine :: IO [Int]
-  print $ solve n as
+  print $ solve' n as
 
 cost = IntMap.fromList $ zip [1..] [2,5,5,4,5,6,3,7,6]
 
+calc :: [Int] -> Integer
 calc as = foldl' (\acc (x, y) -> acc + (fromIntegral x) * y) 0 $ zip (sort as) $ map (10^) [0..]
+
+solve' :: Int -> [Int] -> Integer
+solve' n as = calc . snd $ dp!n
+  where
+    value = IntMap.fromList . filter (\(c, a) -> elem a as) $ zip [2,5,5,4,5,6,3,7,6] [1..] 
+    costs = map (\a -> cost IntMap.! a) as
+    mincost = minimum costs
+    dp = listArray (mincost, n) $ map f [mincost..n] :: Array Int (Int, [Int])
+    f :: Int -> (Int, [Int])
+    f i | i == mincost = (1, [value IntMap.! mincost])
+        | otherwise = select (0, []) $ [ new c | c <- costs]
+        where
+          new c | i - c >= mincost = (prevl+1, x:prevx)
+                | otherwise = (0,[])
+                where
+                  x = value IntMap.! c
+                  (prevl, prevx) = dp!(i - c)
+          
+          select ret [] = ret
+          select (maxl, maxx) (x:xs)
+            | maxl < l = select (l, cx) xs
+            | maxl > l = select (maxl, maxx) xs
+            | maxl == l = if maxx < cx 
+                          then select (l, cx) xs
+                          else select (maxl, maxx) xs
+              where (l, cx) = x 
+
 
 solve :: Int -> [Int] -> Integer
 solve n as = fst $ dp!n
