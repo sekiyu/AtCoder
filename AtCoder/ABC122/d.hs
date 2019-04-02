f247059319425cb7ce261781d4b2f7da1ea5f8f5
utopian <wuthering1118@gmail.com>
2019-04-03 00:18:52 +0900

    ABC122 D

--- AtCoder/ABC122/d.hs
+++ AtCoder/ABC122/d.hs
@@ -1,3 +1,4 @@
+-- ABC122 D
 {-# LANGUAGE BangPatterns #-}
 import Control.Monad
 import Data.Maybe
@@ -19,7 +20,24 @@ main = do
   print $ solve n
 
 -- solve :: Int -> Integer
-solve n = length $ [ w | w <- replicateM n acgt, not . or . map (flip isInfixOf w) $ ngwords ]
+solve n = if n < 4
+          then naive n
+          else foldl' modadd 0 . Map.elems $ foldl' proceed initial [5..n]
+  where
+    oks = [ w | w <- replicateM 4 acgt, not . or . map (flip isInfixOf w) $ ngwords ]
+    initial = Map.fromList $ zip oks (repeat 1)
+    -- proceed :: Map.Map String Int -> Int -> Map.Map String Int
+    proceed m _ = Map.fromList [(ok, calc m ok) | ok <- oks ] 
+    calc m key = foldl' modadd 0 $ map (m Map.!) c
+      where
+        c = [ x |  x <- map (\s -> tail key ++ [s]) acgt, notElem x ngwords, notElem (tail x) ngwords]
+
+naive n = fromIntegral $ length $ [ w | w <- replicateM n acgt, not . or . map (flip isInfixOf w) $ ngwords ]
 
 acgt = "ACGT"
 ngwords = ["AGC", "GAC", "ACG", "ACGC", "AGGC", "ATGC", "AGAC", "AGGC", "AGTC"]
+
+divConst = 10^9 + 7 :: Int
+modadd x y = (x + y) `mod` divConst
+modsub x y = (x - y) `mod` divConst
+modmul x y = ((x `mod` divConst) * (y `mod` divConst)) `mod` divConst
