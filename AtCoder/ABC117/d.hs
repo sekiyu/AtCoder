75fbfe87219103221c41c14d15d55d6f28177a3f
utopian <wuthering1118@gmail.com>
2019-04-06 09:17:52 +0900

    ABC117 D stull WA

--- AtCoder/ABC117/d.hs
+++ AtCoder/ABC117/d.hs
@@ -14,8 +14,40 @@ main :: IO ()
 main = do
   (n:k:_) <- fmap (map read . words) getLine :: IO [Integer]
   as <- fmap (map read . words) getLine :: IO [Integer]
-  print $ solve'' k as
+  print $ solve''' k as
 
+solve''' k as = maximum . map f . (0:) . filter (<=k) . catMaybes $ map go [0..40]
+  -- f $ foldl' g 0 $ takeWhile (<= k) $ map (2^) [0..]
+  -- f . sum . zipWith (*) bins . sumf 0 [] $ zip bins opts
+  -- f . sum . zipWith (*) xs $ map (2^) [0..]
+  where
+    f x = foldr (\a acc -> acc + (a `xor` x)) 0 as
+    bins = takeWhile (<= k) $ map (2^) [0..]
+    kbin = toBinary k
+    opts = map (\b -> if f b > f 0 then 1 else 0) bins
+    fb = sum . zipWith (*) bins
+    go i
+      | i >= length opts || opts !! i == 0 = Nothing 
+      | otherwise = Just . fb $ take i opts ++ drop i kbin
+
+toBinary = reverse . go 
+  where
+    go i
+      | i < 2  = [fromIntegral (i `mod` 2)]
+      | otherwise = (fromIntegral (i `mod` 2)):(go $ i `div` 2)
+
+    {-
+    {-  
+    sumf s acc [] = traceShow s $ reverse acc
+    sumf s acc ((b, opt):bs) = if s + b <= k
+      then sumf (s + if opt == 1 then b else 0) (opt:acc) bs
+      else sumf s (0:acc) bs
+    g acc b = if acc + b > k
+              then acc
+              else if f 0 < f b then acc + b
+                                else acc
+      -}
+      
 -- naive solution 
 solve'' k as = maximum $ map f [0..k]
   where
@@ -73,14 +105,10 @@ count1s m abins = sum . map f $ abins
         | otherwise = sum . zipWith (\b i -> b * 2^i) (take (n - m) bs) $ [(n-1), (n-2)..]
 
 -- toBinary :: Integer -> [Int]
-toBinary = reverse . go 
-  where
-    go i
-      | i < 2  = [fromIntegral (i `mod` 2)]
-      | otherwise = (fromIntegral (i `mod` 2)):(go $ i `div` 2)
 
 slice n ls 
   | m < n = (take (n - m) (repeat 0)) ++ ls
   | otherwise = drop (m - n) ls
   where m = length ls
 
+-}
