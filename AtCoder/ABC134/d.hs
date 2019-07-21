bc0037f68de8483316200edb502823571a7e2ba4
utopian <wuthering1118@gmail.com>
2019-07-21 11:24:17 +0900

    improve ABC134 D

--- AtCoder/ABC134/d.hs
+++ AtCoder/ABC134/d.hs
@@ -17,7 +17,29 @@ main :: IO ()
 main = do
   n <- readLn :: IO Int
   as <- map read . words <$> getLine :: IO [Int]
-  solve n as
+  solve' n as
+
+solve' :: Int -> [Int] -> IO ()
+solve' n as = do
+  let mp = foldl' go IntMap.empty . reverse $ zip as [1..n]
+      m = sum . IntMap.elems $ mp
+  print m
+  if m == 0 
+    then return ()
+    else putStrLn . unwords . map show $ IntMap.keys mp
+  where
+    arr = listArray (1, n) as :: Array Int Int
+    go :: IntMap.IntMap Int -> (Int, Int) -> IntMap.IntMap Int
+    go mp (!a, !i) = newmp
+      where
+        bi = ((sumbs `mod` 2) + arr!i) `mod` 2
+        newmp = if bi /= 0
+          then IntMap.insert i bi mp
+          else mp
+        sumbs = sum $ catMaybes [ IntMap.lookup (i * j) mp | j <- [2..(n `div` i)]]
+
+  
+  
 
 solve :: Int -> [Int] -> IO ()
 solve n as = go (IntMap.empty) . reverse $ zip as [1..n]
