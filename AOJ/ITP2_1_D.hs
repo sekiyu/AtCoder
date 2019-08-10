a151a5071c54cc0b4ae2bf9241b3a5d89e9bdde7
utopian <wuthering1118@gmail.com>
2019-08-10 15:40:28 +0900

    TP2_1_D Vector2

---
+++ AOJ/ITP2_1_D.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE BangPatterns #-}
+import Control.Monad
+import Data.Maybe
+import Debug.Trace
+import Data.List
+import qualified Data.Map.Strict as Map
+import qualified Data.IntMap.Strict as IntMap
+import qualified Data.Set as Set
+import qualified Data.IntSet as IntSet
+import Data.Functor
+import Data.Array
+-- import Data.Array.Unboxed
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.IO
+import Data.Char
+import qualified Data.ByteString.Char8 as B
+import Data.Maybe (fromJust)
+readInt = fst . fromJust . B.readInt
+readInts = map (fst . fromJust . B.readInt) . B.words <$> B.getLine :: IO [Int]
+readIntegers = map (fst . fromJust . B.readInteger) . B.words <$> B.getLine :: IO [Integer]
+read2dInts = map (map (fst . fromJust . B.readInt) . B.words) . B.lines <$> B.getContents
+readVInts = map (fst . fromJust . B.readInt) . B.lines <$> B.getContents
+
+main :: IO ()
+main = do
+  (n:q:_) <- map read . words <$> getLine :: IO [Int]
+  qs <- read2dInts
+  -- qs <- replicateM q $ map read . words <$> getLine :: IO [[Int]]
+  solve n qs
+
+solve :: Int -> [[Int]] -> IO ()
+solve n qs = do
+  arr <- newArray (0, n) [] :: IO (IOArray Int [Int])
+  forM_ qs $ \(query:t:qss) -> do
+    at <- readArray arr t :: IO [Int]
+    case query of
+      0 -> do
+        let ([x]) = qss 
+        -- writeArray :: (MArray a e m, Ix i) => a i e -> i -> e -> m ()
+        writeArray arr t (x:at) :: IO () 
+        return arr
+      1 -> do
+        putStrLn . unwords . map show $ reverse at 
+        return arr
+      2 -> do
+        writeArray arr t []
+        return arr
+        
